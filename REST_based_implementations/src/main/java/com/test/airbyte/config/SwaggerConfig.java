9c1e024063990cdf03dec4094b8b98f9ef94cea5
Mohamed Rilwan <talk_to_rilwan@outlook.com>
2025-12-31 21:19:51 +0530

    updated swagger doc

---
+++ REST_based_implementations/src/main/java/com/test/airbyte/config/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package com.test.airbyte.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Value("${swagger.server-url}")
+    private String swaggerServerUrl;
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Airbyte implementation API Docs")
+                        .description("API Documentation for Airbyte functionalities")
+                        .version("1.0.0"))
+                .servers(List.of(
+                        new Server()
+                                .url(swaggerServerUrl)
+                ));
+    }
+}
